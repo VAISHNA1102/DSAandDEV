6c2ae65085a65c680854441efb87c16bb3be5165
karanwaghh <karanwagh7860@gmail.com>
2024-07-01 19:43:05 +0530

    home page done

--- src/components/Testimonals.js
+++ src/components/Testimonals.js
@@ -3,8 +3,8 @@ import Reviews from './Reviews'
 
 const Testimonals = () => {
   return (
-    <div className=''>
-      <h4 className='text-center inconsolataFontFamily font-light text-white md:text-lg'>Testimonals</h4>
+    <div className='mt-16'>
+      <h4 className='text-center inconsolataFontFamily font-light text-white md:text-2xl uppercase'>Testimonals</h4>
       <h1 className='manropeFontFamily font-bold text-2xl md:text-4xl text-center mt-4 text-white'>First Time User Reviews</h1>
       <Reviews/>
     </div>
